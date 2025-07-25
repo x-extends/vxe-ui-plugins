677dda410e487689befaa5205543d23fba9c2b09
xuliangzhan <xu_liangzhan@163.com>
2025-07-25 16:22:54 +0800

    plugin-validator 支持19号码

--- plugin-validator/src/index.ts
+++ plugin-validator/src/index.ts
@@ -1,14 +1,17 @@
 import type { VxeUIExport, VxeFormDefines } from 'vxe-pc-ui'
 import type { VxeTableDefines, VxeGlobalValidatorsHandles } from 'vxe-table'
+import type { VxeUIPluginValidatorOptions, VxeUIPluginValidatorOptionCustomRegExp } from '../types'
 
 let VxeUI: VxeUIExport
 
-const reMobile = /^0?(13[0-9]|15[012356789]|17[013678]|18[0-9]|14[57])[0-9]{8}$/
+const reMobile = /^0?(13[0-9]|15[012356789]|17[013678]|18[0-9]|19[0-3,5-9]|14[57])[0-9]{8}$/
 const reEmail = /^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/
 const reIdentityCard = /^[1-9]\d{5}(18|19|20)\d{2}((0[1-9])|(1[0-2]))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$/
 const reIp = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
 const reUrl = /^((https?|ftp|file):\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([\w.-]*)*\/?$/
 const rePlateNumber = /^[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}[A-Z0-9]{4}[A-Z0-9挂学警港澳]{1}$/
+
+const customRegExpMaps: VxeUIPluginValidatorOptionCustomRegExp = {}
 
 function createValidRule (checkMethod: (value: any, rule: VxeTableDefines.ValidatorRule | VxeFormDefines.FormRule) => any) {
   const validOpts: VxeGlobalValidatorsHandles.ValidatorsOptions = {
@@ -23,22 +26,36 @@ function createValidRule (checkMethod: (value: any, rule: VxeTableDefines.Valida
 }
 
 /**
+ * 设置参数
+ * @param options 参数
+ */
+function pluginConfig (options: VxeUIPluginValidatorOptions) {
+  if (options) {
+    Object.assign(customRegExpMaps, options.customRegExp)
+  }
+}
+
+/**
  * Vxe UI 的扩展插件，提供常用的校验配置
  */
 export const VxeUIPluginValidator = {
-  install (core: VxeUIExport) {
+  setConfig: pluginConfig,
+  install (core: VxeUIExport, options?: VxeUIPluginValidatorOptions) {
     VxeUI = core
 
     // 检查版本
     if (!/^(4)\./.test(VxeUI.uiVersion)) {
       console.error('[VUE_APP_VXE_PLUGIN_VERSION] Requires VUE_APP_VXE_TABLE_VERSION+ version. VUE_APP_VXE_PLUGIN_DESCRIBE')
     }
+    if (options) {
+      pluginConfig(options)
+    }
 
     VxeUI.validators.mixin({
       // 手机号码，11位
       MOBILE_NUMBER: createValidRule((value, rule) => {
         if (rule.required || value) {
-          if (!reMobile.test(value)) {
+          if (!(customRegExpMaps.mobile || reMobile).test(value)) {
             return new Error('请输入正确的手机号')
           }
         }
@@ -46,7 +63,7 @@ export const VxeUIPluginValidator = {
       // 邮箱
       EMAIL_ADDRESS: createValidRule((value, rule) => {
         if (rule.required || value) {
-          if (!reEmail.test(value)) {
+          if (!(customRegExpMaps.email || reEmail).test(value)) {
             return new Error('请输入正确的邮箱账号')
           }
         }
@@ -54,7 +71,7 @@ export const VxeUIPluginValidator = {
       // 身份证号码
       IDENTITY_CARD: createValidRule((value, rule) => {
         if (rule.required || value) {
-          if (!reIdentityCard.test(value)) {
+          if (!(customRegExpMaps.identityCard || reIdentityCard).test(value)) {
             return new Error('请输入正确的身份证号')
           }
         }
@@ -62,7 +79,7 @@ export const VxeUIPluginValidator = {
       // IP地址
       IP_ADDRESS: createValidRule((value, rule) => {
         if (rule.required || value) {
-          if (!reIp.test(value)) {
+          if (!(customRegExpMaps.ip || reIp).test(value)) {
             return new Error('请输入正确的IP地址')
           }
         }
@@ -70,7 +87,7 @@ export const VxeUIPluginValidator = {
       // URL
       URL: createValidRule((value, rule) => {
         if (rule.required || value) {
-          if (!reUrl.test(value)) {
+          if (!(customRegExpMaps.url || reUrl).test(value)) {
             return new Error('请输入正确的URL地址')
           }
         }
@@ -78,7 +95,7 @@ export const VxeUIPluginValidator = {
       // 车牌号
       PLATE_NUMBER: createValidRule((value, rule) => {
         if (rule.required || value) {
-          if (!rePlateNumber.test(value)) {
+          if (!(customRegExpMaps.plateNumber || rePlateNumber).test(value)) {
             return new Error('请输入正确的车牌号')
           }
         }
--- plugin-validator/types/index.d.ts
+++ plugin-validator/types/index.d.ts
@@ -1,10 +1,23 @@
 import { VxeUIExport } from 'vxe-pc-ui'
 
+export interface VxeUIPluginValidatorOptionCustomRegExp {
+  mobile?: RegExp
+  email?: RegExp
+  identityCard?: RegExp
+  ip?: RegExp
+  url?: RegExp
+  plateNumber?: RegExp
+}
+
+export interface VxeUIPluginValidatorOptions {
+  customRegExp?: VxeUIPluginValidatorOptionCustomRegExp
+}
+
 /**
  * Vxe UI 的扩展插件，提供常用的校验配置
  */
 export declare const VxeUIPluginValidator: {
-  install (VxeUI: VxeUIExport): void
+  install (VxeUI: VxeUIExport, options?: VxeUIPluginValidatorOptions): void
 }
 
 export default VxeUIPluginValidator
